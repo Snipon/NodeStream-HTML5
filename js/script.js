339b744f7e1dfd0dbee2046b8056675799fdce3c
FreddieK <freddiek@kth.se>
2011-08-22 16:49:10 +0200

    language agnostic, added kicker field to full node

--- js/script.js
+++ js/script.js
@@ -4,25 +4,27 @@ $(document).ready(function(){
       
   $.getJSON(ns_url, function(data){
     
-    //foreach data.nid...
-    // get created, title, 
-
     console.log(data);
     
+    var lang = data.language;
+    
+    /* Create list representation */
     var list_data = '';
     list_data += "<a href='#nid_" + data.nid + "'><h2>" + data.title + '</h2></a>';
     list_data += '<span>' + data.created + '</span>'; // to better format...
-    list_data += '<p>' + data.field_ns_article_lead.und[0].safe_value + '</p>';
+    list_data += '<p>' + data.field_ns_article_lead[lang][0].safe_value + '</p>';
 
     $('<div/>',{
       'class': 'news-list-item',
       html: list_data
       }).appendTo('#home');
 
+    /* Construct full node representation */
     var full_data = '';    
     full_data += '<h2>' + data.title + '</h2>';
     full_data += '<span>' + data.created + '</span>'; // to better format...
-    full_data += '<p>' + data.field_ns_article_body.und[0].safe_value + '</p>';
+    full_data += '<p><em>' + data.field_ns_article_kicker[lang][0].safe_value + '</em></p>';
+    full_data += '<p>' + data.field_ns_article_body[lang][0].safe_value + '</p>';
     
     $('<div/>',{
       'class': 'news-list-item',
