13955b5af0315d87eebba6eddf8f75bb2940783b
FreddieK <freddiek@kth.se>
2011-08-22 17:21:31 +0200

    starting to separate model from controller

--- js/models/articles.js
+++ js/models/articles.js
@@ -1,42 +1,27 @@
-$(document).ready(function(){
-      
-  $.getJSON(ns.url, function(data){
-    
-    console.log(data);
-    
-    var lang = data.language;
-    
+ns.articles = {
+  
+  list: 'test',
+  items: ['1', '23'],
+
+  create: function(data){
+  
+    return [this.list, this.items];
+  },
+  
+  build_list: function(){
     /* Create list representation */
     var list_data = '';
     list_data += "<a href='#nid_" + data.nid + "'><h2>" + data.title + '</h2></a>';
     list_data += '<span>' + data.created + '</span>'; // to better format...
     list_data += '<p>' + data.field_ns_article_lead[lang][0].safe_value + '</p>';
-    
-    $('<div/>',{
-      'class': 'news-list-item',
-      html: list_data
-      }).appendTo('#home');
-
+  },
+  
+  build_item: function(){
     /* Construct full node representation */
     var full_data = '';    
     full_data += '<h2>' + data.title + '</h2>';
     full_data += '<span>' + data.created + '</span>'; // to better format...
     full_data += '<p><em>' + data.field_ns_article_kicker[lang][0].safe_value + '</em></p>';
     full_data += '<p>' + data.field_ns_article_body[lang][0].safe_value + '</p>';
-    
-    $('<div/>',{
-      'class': 'news-item',
-      'id': 'nid_' + data.nid,
-      html: full_data
-      }).appendTo('body');
-  });
-  
-  // returns array with two items, first the formatted HTML list of articles for the menu, secondly all the items
-/*  var new_data = ns.articles.create();
-  
-  $('<div/>',{
-    'class': 'news-list-item',
-    'id': 'nid_new',
-    html: new_data.join('')
-    }).appendTo('body');   */
-});
+  },
+}
