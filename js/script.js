844da651d177058ada13abb3c3f05f827684bb3e
FreddieK <freddiek@kth.se>
2011-08-22 22:10:16 +0200

    float left as a basic layout

--- js/script.js
+++ js/script.js
@@ -9,24 +9,10 @@ $(document).ready(function(){
       html: formatted_articles_data[0]
       }).appendTo('#content-teaser-list');
 
-    $('.news-list-item a').bind('click', function(event){
-      
-    // '#shown-article'
-    $("#shown-article").replaceWith("<div id='shown-article'>" + ns.articles.items[$(this).attr('href')] + '</div>');
-/*    $('<div/>',{
-      'class': 'news-item',
-      html: ns.articles.items[$(this).attr('href')]
-      }).appendTo('#content'); */
-      
-      event.preventDefault();
-            
+    $('.news-list-item a').bind('click', function(event){      
+      $("#shown-article").replaceWith("<div id='shown-article'>" + ns.articles.items[$(this).attr('href')] + '</div>');
+      event.preventDefault();     
     });
-
-/*    $('<div/>',{
-      'class': 'news-item',
-      html: formatted_articles_data[1].join('')
-      }).appendTo('#content');
-*/
-      
+    
   });
 });
