3a5559fcb0b7bbf0601f2f03a17a10c11bb69767
FreddieK <freddiek@kth.se>
2011-08-22 16:18:49 +0200

    better example

--- js/script.js
+++ js/script.js
@@ -4,12 +4,19 @@ $(document).ready(function(){
       
   $.getJSON(ns_url, function(data){
     
+    //foreach data.nid...
+    // get created, title, 
+    // 
+    
     var formatted_data = '';
     formatted_data += '<h2>' + data.title + '</h2>';
-//    formatted_data += '<div>' + data.body + '</div>';
+    formatted_data += '<div>' + data.created + '</div>'; // to better format...
+    formatted_data += '<p>' + data.field_ns_article_lead.und[0].safe_value + '</p>'; // to better format...
+    
+    console.log(data);
 
     $('<div/>', {
-      'class': 'news-list',
+      'class': 'news-list-item',
       html: formatted_data
       }).appendTo('.content');
       
